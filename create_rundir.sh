845fc2ec4c84594e6eb81dd5dc8306eeacbfa594
ethoX <58401466+eth0-02@users.noreply.github.com>
2026-08-26 12:54:33 +0300

    chore: sanitize hardcoded local paths in create_rundir.sh

--- create_rundir.sh
+++ create_rundir.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd GCClassic/run
-rm -rf /home/kostas/GEOS-Chem/Runs/Olkaria_Carbon
-printf "/home/kostas/GEOS-Chem/Data\n3\n1\n2\ny\n4\n6\n2\n/home/kostas/GEOS-Chem/Runs/Olkaria_Carbon\ny\n" | ./createRunDir.sh
+rm -rf $HOME/GEOS-Chem/Runs/Olkaria_Carbon
+printf "$HOME/GEOS-Chem/Data\n3\n1\n2\ny\n4\n6\n2\n$HOME/GEOS-Chem/Runs/Olkaria_Carbon\ny\n" | ./createRunDir.sh
