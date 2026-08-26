e05a75f6626c7cbc7cc7fe0759347119ed085d9b
ethoX <58401466+eth0-02@users.noreply.github.com>
2026-08-26 12:54:37 +0300

    chore: sanitize hardcoded local paths in final_run.sh

--- final_run.sh
+++ final_run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-source /home/kostas/env_setup.sh
-cd /home/kostas/GEOS-Chem/Runs/Olkaria_Carbon
-echo "Starting GEOS-Chem at $(date)" >> /home/kostas/GEOS-Chem/Runs/Olkaria_Carbon/simulation_jan2019.log
-./gcclassic >> /home/kostas/GEOS-Chem/Runs/Olkaria_Carbon/simulation_jan2019.log 2>&1
-echo "GEOS-Chem finished at $(date)" >> /home/kostas/GEOS-Chem/Runs/Olkaria_Carbon/simulation_jan2019.log
+source $HOME/env_setup.sh
+cd $HOME/GEOS-Chem/Runs/Olkaria_Carbon
+echo "Starting GEOS-Chem at $(date)" >> $HOME/GEOS-Chem/Runs/Olkaria_Carbon/simulation_jan2019.log
+./gcclassic >> $HOME/GEOS-Chem/Runs/Olkaria_Carbon/simulation_jan2019.log 2>&1
+echo "GEOS-Chem finished at $(date)" >> $HOME/GEOS-Chem/Runs/Olkaria_Carbon/simulation_jan2019.log
